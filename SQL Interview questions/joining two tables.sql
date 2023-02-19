2cdbdb6ea1983bdf1dd9fb9d3e9a3fa91a8314fe
Sulekha mohamud <sulekhasabah@gmail.com>
2023-02-19 14:40:08 +0100

    Update joining two tables.sql

--- SQL Interview questions/joining two tables.sql
+++ SQL Interview questions/joining two tables.sql
@@ -37,4 +37,4 @@ INSERT INTO Address (AddressId, PersonId, City,State) VALUES
 # Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of thosepeople:
 #FirstName, LastName, City, State
 select FirstName, LastName, City, State
-from Person as p left join Address as a on p.PersonId = a.PersonId;
+from Person as p left join Address as a on p.PersonId = a.PersonId;;
