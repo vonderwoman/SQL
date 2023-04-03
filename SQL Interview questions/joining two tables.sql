a7115c4ee9723521d378c0d0ba9eebbeca078e12
Sulekha mohamud <sulekhasabah@gmail.com>
2023-04-03 20:48:01 +0200

    Create joining two tables.sql

--- SQL Interview questions/joining two tables.sql
+++ SQL Interview questions/joining two tables.sql
@@ -34,9 +34,9 @@ INSERT INTO Address (AddressId, PersonId, City,State) VALUES
 (18,24,'Newyork','New Jersey'),
 (19,25,'Newyork','New Jersey');
 
-# combine this two tables
+# combine this two tables together
 # Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of thosepeople:
 #FirstName, LastName, City,, State
 select FirstName, LastName, City, State
-from Person as p left join Address as a on p.PersonId = a.PersonId;:
+from Person as p left join Address as a on p.PersonId = a.PersonId;
 
